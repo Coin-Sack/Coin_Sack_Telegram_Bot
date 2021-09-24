8af660a3d9215537ff3d343f456a0ddea33631d0
Stew <stew@stewhering.com>
2021-09-24 00:59:08 -0400

    Update command structure

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ let contextsGettingUpdates = [];
 
 // Global Bot Commands
 bot.command('about', async function(context) {
-    context.reply(fs.readFileSync('./replies/about.txt').toString());
+    context.replyWithMarkdown(fs.readFileSync('./replies/about.md').toString());
 })
 bot.command('website', async function(context) {
     context.reply(fs.readFileSync('./replies/website.txt').toString());
@@ -52,63 +52,34 @@ bot.command('issue', async function(context) {
 
 
 // Group Chat Specific Bot Commands
-bot.groupChat([
+Telegraf.groupChat([
     bot.command('socials', async function(context) {
         context.reply(fs.readFileSync('./replies/group/socials.txt').toString());
-        log("Get Socials (group chat)");
     }),
     bot.command('commands', async function(context) {
         context.reply(fs.readFileSync('./replies/group/commands.txt').toString());
-        LOG("Get Commands (group chat)");
     }),
-    bot.admin([
-        bot.command('start-updates', async function(context) {
-            var isContextGettingUpdates = false;
-            contextsGettingUpdates.forEach(contextGettingUpdates => {
-                if(contextGettingUpdates.message.chat.id == context.message.chat.id){
-                    isContextGettingUpdates = true;
-                }
-            });
-            if(!isContextGettingUpdates){
-                contextsGettingUpdates.push(context);
-                context.reply(fs.readFileSync('./replies/group/start-updates.txt'));
-                LOG("Start Scheduled Updates (group chat)");
-            }
-        }),
-        bot.command('stop-updates', async function(context) {
-            var isContextGettingUpdates = false;
-            var contextGettingUpdatesIndex = 0;
-            contextsGettingUpdates.forEach((contextGettingUpdates, index) => {
-                if(contextGettingUpdates.message.chat.id == context.message.chat.id){
-                    isContextGettingUpdates = true;
-                    contextGettingUpdatesIndex = index;
-                }
-            });
-            if(isContextGettingUpdates){
-                contextsGettingUpdates.splice(contextGettingUpdatesIndex, 1);
-                context.reply(fs.readFileSync('./replies/group/stop-updates.txt'));
-                LOG("Stop Scheduled Updates (group chat)");
-            }
-        })
-    ])
+    
 ]);
 
 
 // Private Chat Specific Bot Commands
-bot.privateChat([
+Telegraf.privateChat([
     bot.command('socials', async function(context) {
         context.reply(fs.readFileSync('./replies/private/socials.txt').toString());
-        LOG("Get Socials (private chat)");
     }),
     bot.command('tetegram', async function(context) {
         context.reply(fs.readFileSync('./replies/private/telegram.txt').toString());
-        LOG("Get Telegram (private chat)")
     }),
     bot.command('commands', async function(context) {
         context.reply(fs.readFileSync('./replies/private/commands.txt').toString());
-        LOG("Get Commands (private chat)")
-    }),
-    bot.command('start-updates', async function(context) {
+    })
+]);
+
+
+// Chat Admin / Creator Bot Commands
+Telegraf.admin([
+    bot.command('start', async function(context) {
         var isContextGettingUpdates = false;
         contextsGettingUpdates.forEach(contextGettingUpdates => {
             if(contextGettingUpdates.message.chat.id == context.message.chat.id){
@@ -117,11 +88,10 @@ bot.privateChat([
         });
         if(!isContextGettingUpdates){
             contextsGettingUpdates.push(context);
-            context.reply(fs.readFileSync('./replies/private/start-updates.txt'));
-            LOG("Start Scheduled Updates (private chat)");
+            context.reply(fs.readFileSync('./replies/admin/start-updates.txt'));
         }
     }),
-    bot.command('stop-updates', async function(context) {
+    bot.command('stop', async function(context) {
         var isContextGettingUpdates = false;
         var contextGettingUpdatesIndex = 0;
         contextsGettingUpdates.forEach((contextGettingUpdates, index) => {
@@ -132,12 +102,10 @@ bot.privateChat([
         });
         if(isContextGettingUpdates){
             contextsGettingUpdates.splice(contextGettingUpdatesIndex, 1);
-            context.reply(fs.readFileSync('./replies/private/stop-updates.txt'));
-            LOG("Stop Scheduled Updates (private chat)");
+            context.reply(fs.readFileSync('./replies/admin/stop-updates.txt'));
         }
     })
 ]);
-
 
 const sendContextUpdates = function() {
     LOG("Send Scheduled Updates");
---
+++ replies/about.md
@@ -0,0 +1,17 @@
+## About Coin Sack
+
+_Coin Sack was created with the belief that there need to be stronger, more trustworthy, and more worthwhile projects built throughout the deFi space._
+
+_We decided to take this into our own hands by launching Coin Sack; a BEP-20 token featuring battle tested tokenomics, an innovative future roadmap, and a trustworthy team that cares for its investors._
+
+**CS token features:**
+- 100 billion token total supply / 3 decimal fungibility (WE CAN ACTUALLY HIT $1)
+- 15% Buy Fees / 20% Sell Fees
+    - 8% / 10% contributed to buyback war chest
+    - 4% / 5% distributed as refi holder rewards
+    - 3% / 5% contributed to project operations
+- No fees applied when transfering tokens between wallets
+- Integrity, Community, Innovation, and Performance are at the project's core
+- Doxed, trustworthy team
+
+Learn more online at [Coin-Sack.com](https://coin-sack.com/)!
