d8981c7ae496ec494da1a9cfbbdec5078d8fdb08
Stew <stew@stewhering.com>
2021-09-23 22:17:16 -0400

    Update command structure & update schedule

--- bot.js
+++ bot.js
@@ -1,60 +1,117 @@
 /* Coin Sack Telegram Bot */
 
-/* To-do:
-    - add timed updates messages
-    - automatic welcome messages
-    - 
-
-*/
-
 const fs = require('fs');
 const { Telegraf } = require('telegraf');
 const cron = require('node-cron');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-let botContext = undefined;
+let updateContexts = [];
 
-bot.command('start', context => {
-    context.reply("Hello, I'm the Coin Sack bot.\n\nIt is good to be alive!");
-    context.reply("Use my commands to get up-to-date information about Coin Sack:\n\n/about - view up to date information about Coin Sack\n/website - view the Coin Sack website\n/contract - view the Coin Sack smart contract address\n/whitepaper - read the Coin Sack whitepaper\n/commands - view a list of my commands");
-
-    if(botContext == undefined){
-       botContext = context;
-    }
-});
-
-bot.command('website', context => {
-    context.reply("The Coin Sack Website:");
-    context.reply("https://coin-sack.com/");
-});
-
-bot.command("contract", context => {
-    context.reply("The Coin Sack Smart Contract:");
-    context.reply("0x125Ce3f13950C5fA94397927F88C352FdED680Ad\nhttps://bscscan.com/token/0x125Ce3f13950C5fA94397927F88C352FdED680Ad");
-});
-
-bot.command("whitepaper", context => {
-    context.reply("Read The Coin Sack Whitepaper:");
-    context.reply("https://www.coin-sack.com/whitepaper");
+bot.command('about', context => {
+    context.reply(fs.readFileSync('./replies/about.txt').toString());
 })
 
-bot.command("about", context => {
-    context.reply(fs.readFileSync('./replies/about.txt').toString());
+bot.command('website', context => {
+    
+});
+
+bot.command('whitepaper', context => {
+    
+});
+
+bot.command('roadmap', context => {
+
+});
+
+bot.command('socials', context => {
+
+});
+
+bot.command('instagram', context => {
+
+});
+
+bot.command('twitter', context => {
+
+});
+
+bot.command('github', context => {
+
+});
+
+bot.command('token', context => {
+
+});
+
+bot.command('price', context => {
+
+});
+
+bot.command('contract', context => {
+
+});
+
+bot.command('introduce', context => {
+
+});
+
+bot.command('start', context => {
+    
+});
+
+bot.command('stop', context => {
+
 });
 
 bot.command("commands", context => {
-    context.reply("Use my commands to get up-to-date information about Coin Sack:\n\n/about - view up to date information about Coin Sack\n/website - view the Coin Sack website\n/contract - view the Coin Sack smart contract address\n/whitepaper - read the Coin Sack whitepaper\n/commands - view a list of my commands");
-})
+    
+});
+
+bot.command("issue", context => {
+
+});
+
 
 bot.launch();
 
-cron.schedule('10 3,6,9,12,15,18,21 * * *', () => {
-  botContext.telegram.sendMessage(botContext.message.chat.id, fs.readFileSync('./updates/1.txt').toString());
+cron.schedule('6 7,18,23 * * *', () => {
+    var possibleUpdates = [];
+    fs.readirSync('./updates').forEach((updateFile, index) => {
+        possibleUpdates.push(fs.readFileSync(updateFile).toString());
+    });
+
+    var chosenUpdate = possibleUpdates[Math.floor(Math.random()*possibleUpdates.length)];
+
+    updateContexts.forEach((context) => {
+        context.telegram.sendMessage(context.message.chat.id, chosenUpdate);
+    });
 });
 
-cron.schedule('20 4,7,10,13,16,19,22 * * *', () => {
-  botContext.telegram.sendMessage(botContext.message.chat.id, fs.readFileSync('./updates/2.txt').toString());
+cron.schedule('53 10,21 * * *', () => {
+    var possibleUpdates = [];
+    fs.readirSync('./updates').forEach((updateFile, index) => {
+        possibleUpdates.push(fs.readFileSync(updateFile).toString());
+    });
+
+    var chosenUpdate = possibleUpdates[Math.floor(Math.random()*possibleUpdates.length)];
+
+    updateContexts.forEach((context) => {
+        context.telegram.sendMessage(context.message.chat.id, chosenUpdate);
+    });
+});
+
+cron.schedule('27 12,15 * * *', () => {
+    var possibleUpdates = [];
+    fs.readirSync('./updates').forEach((updateFile, index) => {
+        possibleUpdates.push(fs.readFileSync(updateFile).toString());
+    });
+
+    var chosenUpdate = possibleUpdates[Math.floor(Math.random()*possibleUpdates.length)];
+
+    updateContexts.forEach((context) => {
+        context.telegram.sendMessage(context.message.chat.id, chosenUpdate);
+    });
 });
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
